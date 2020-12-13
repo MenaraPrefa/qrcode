4d860da7488c0f307462cccbe021c1de74f9143e
AmineBendriouchDev <aminebendriouch.dev@gmail.com>
2020-12-13 21:09:18 +0100

    Last Commit

---
+++ js/app.js
@@ -0,0 +1,34 @@
+document.getElementById('bg').classList.add('close');
+setTimeout(function () {
+  //   document.getElementById('bg').classList.add('close');
+  var scanner = new Instascan.Scanner({
+    video: document.getElementById('preview'),
+    scanPeriod: 5,
+    mirror: false,
+  });
+
+  scanner.addListener('scan', function (content) {
+    content = content.replace(' TRIAL', '');
+    window.location.href =
+      'http://105.155.253.15:12095/qrcode_mobile/valider.php?qrcode=' + content;
+  });
+
+  Instascan.Camera.getCameras()
+    .then(function (cameras) {
+      if (cameras.length > 0) {
+        // Change camera 0=front 1=back
+        scanner.start(cameras[1]);
+      } else {
+        console.error('No cameras found.');
+        alert('No cameras found.');
+      }
+    })
+    .catch(function (e) {
+      console.error(e);
+      alert(e);
+    });
+
+  function closeDetail() {
+    document.getElementById('detail').classList.remove('show-detail');
+  }
+}, 1000);
